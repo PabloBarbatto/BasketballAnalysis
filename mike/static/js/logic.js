d46ae75a599f8ff579e2f1f331bcbc4a4acd429a
mlubin <lubinmichael86@gmail.com>
2019-03-08 22:28:02 -0500

    index update

--- mike/static/js/logic.js
+++ mike/static/js/logic.js
@@ -1,4 +1,4 @@
-var countries = 
+var players = 
 ["Aaron Gordon",
 "Al-Farouq Aminu",
 "Al Horford",
@@ -235,59 +235,9 @@ var countries =
 "Zach Randolph"]
 
 
-
 var theThing = document.querySelector("#bball");
 var container = document.querySelector("#contentContainer");
- 
 
-
-container.addEventListener("click", getClickPosition, false);
-
-
- 
-
-
-
-function getClickPosition(e) {
-    var parentPosition = getPosition(e.currentTarget);
-    var xPosition = e.clientX - parentPosition.x - (theThing.clientWidth / 2);
-    var yPosition = e.clientY - parentPosition.y - (theThing.clientHeight / 2);
-     
-    theThing.style.left = xPosition + "px";
-    theThing.style.top = yPosition + "px";
-
-    //###### Logs Location #######
-var location = console.log("X",xPosition/10, "Y",yPosition/10)
-  }
-
-// Helper function to get an element's exact position
-function getPosition(el) {
-  var xPos = 0;
-  var yPos = 0;
- 
-  while (el) {
-    if (el.tagName == "BODY") {
-      // deal with browser quirks with body/window/document and page scroll
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
- 
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      // for all other non-BODY elements
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
- 
-    el = el.offsetParent;
-    
-   }
-  return {
-    x: xPos,
-    y: yPos
-  };
-
-}
 
 function autocomplete(inp, arr) {
     /*the autocomplete function takes two arguments,
